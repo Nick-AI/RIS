e3d5767ed41387339bd1e2b9765770c982e26d5f
hillsethan <hillsethan@gmail.com>
2018-02-14 14:03:16 -0500

    Update SQLTables.sql

--- SQLTables.sql
+++ SQLTables.sql
@@ -10,15 +10,19 @@
 create table if not exists login(
  username varchar(10),
  password varchar(10),
- admin boolean);
+ level boolean);
  
+# level defines access
+
  Insert into table login
  
  create table if not exists patientInfo(
  fname varchar(20),
  lname varchar(25),
  ID int,
- DOB date);
+ DOB date,
+ gender varchar(3)
+ );
  
  create table if not exists PACS(
  ID int,
@@ -28,7 +32,9 @@ create table if not exists login(
  create table if not exists billing(
   ID int PRIMARY KEY,
   totalAmount long,
-  dueDate date);
+  dueDate date,
+  insurance varchar(20),
+  insureID int);
   
  
  
