bc76a90d415d403c0e38dbb8f5877dfc785466ee
hillsethan <hillsethan@gmail.com>
2018-02-14 13:01:43 -0500

    Create SQLTables.sql

---
+++ SQLTables.sql
@@ -0,0 +1,28 @@
+###### SQL Database
+# NAME: RISystem
+#TABLES: 
+#  - Login
+#  - patientInfo
+#  - billing
+#  - PACS
+
+
+create table if not exists login(
+ username varchar(10),
+ password varchar(10),
+ admin boolean);
+ 
+ Insert into table login
+ 
+ create table if not exists patientInfo(
+ fname varchar(20),
+ lname varchar(25),
+ ID int,
+ DOB date);
+ 
+ create table if not exists PACS(
+ ID int,
+ imageDate date,
+ image varBinary(Max));
+ 
+ 
