00e762795a7da2dc497ee975b35a41474e7340b3
Nick-AI <31321017+Nick-AI@users.noreply.github.com>
2018-04-11 14:23:43 -0400

    Update irsystem.sql

--- irsystem.sql
+++ irsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2018 at 08:28 PM
+-- Generation Time: Apr 11, 2018 at 08:22 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -33,20 +33,6 @@ CREATE TABLE `billing` (
   `patientID` int(10) NOT NULL,
   `procedureList` text NOT NULL,
   `inProgress` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `file`
---
-
-CREATE TABLE `file` (
-  `patientID` int(10) NOT NULL,
-  `refPhysicianID` int(10) NOT NULL,
-  `lastModifiedID` int(10) NOT NULL,
-  `lastModifiedDate` datetime NOT NULL,
-  `fileID` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -90,13 +76,22 @@ CREATE TABLE `patient` (
   `lName` varchar(255) NOT NULL,
   `dateOfBirth` date NOT NULL,
   `gender` varchar(255) NOT NULL,
-  `phoneNum` int(14) NOT NULL,
+  `phoneNum` varchar(14) NOT NULL,
   `addressOne` varchar(255) NOT NULL,
   `addressTwo` varchar(255) NOT NULL,
   `addressCity` varchar(255) NOT NULL,
   `addressState` varchar(2) NOT NULL,
-  `addressZip` int(5) NOT NULL
+  `addressZip` varchar(5) NOT NULL,
+  `refPhysicianID` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`patientID`, `fName`, `lName`, `dateOfBirth`, `gender`, `phoneNum`, `addressOne`, `addressTwo`, `addressCity`, `addressState`, `addressZip`, `refPhysicianID`) VALUES
+(1, 'Bill', 'Doe', '1999-01-01', 'Male', '100-100-1010', '1 Street', 'Box 1', 'City', 'AL', '10100', 23445),
+(1000000000, 'Jane', 'Doe', '1999-12-12', 'Female', '100-200-3000', '1 Road', 'Apartment 1', 'City', 'AL', '11111', 23445);
 
 -- --------------------------------------------------------
 
@@ -126,6 +121,13 @@ CREATE TABLE `radorder` (
   `orderID` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `radorder`
+--
+
+INSERT INTO `radorder` (`refPhysicianID`, `modality`, `specification`, `patientID`, `patientFName`, `patientLName`, `orderID`) VALUES
+(23445, 'XRAY', 'Something something shoulder something something', 1, 'Bill', 'Doe', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -137,6 +139,14 @@ CREATE TABLE `refphysician` (
   `refPhysicianID` int(10) NOT NULL,
   `fName` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `refphysician`
+--
+
+INSERT INTO `refphysician` (`lName`, `refPhysicianID`, `fName`) VALUES
+('Man', 1, 'Mister'),
+('Woman', 1000000000, 'Misses');
 
 -- --------------------------------------------------------
 
@@ -150,7 +160,7 @@ CREATE TABLE `report` (
   `beingModified` tinyint(1) NOT NULL,
   `lastModifiedDate` date NOT NULL,
   `lastModifiedID` int(10) NOT NULL,
-  `fileID` int(10) NOT NULL
+  `patientID` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -168,6 +178,15 @@ CREATE TABLE `schedule` (
   `orderID` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `schedule`
+--
+
+INSERT INTO `schedule` (`procType`, `procTime`, `modality`, `patientID`, `procID`, `orderID`) VALUES
+('blabla shoulder blabla', '04/04/2011-12', 'XRAY', 1, 1, 1),
+('bla bla ankle', '05/05/2020-12', 'MRI', 1000000000, 2, 1),
+('blabla ribs', '10/10/2019', 'MRI', 1000000000, 3, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -183,6 +202,17 @@ CREATE TABLE `staff` (
   `dateOfBirth` date NOT NULL,
   `SSN` int(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`staffID`, `staffType`, `salary`, `fName`, `lName`, `dateOfBirth`, `SSN`) VALUES
+(23445, 'Referring Physician', '0', 'Ref', 'Phys', '0000-00-00', 0),
+(300000000, 'Technician', '50', 'Tech', 'McTechface', '1987-06-05', 111111111),
+(400000000, 'Reception', '30', 'Desk', 'Front', '1991-01-09', 999999999),
+(1000000000, 'Nurse', '35', 'Nurse', 'McNurseface', '1900-01-01', 123121234),
+(2000000000, 'Radiologist', '50', 'Rad', 'Iologist', '1919-01-09', 321214321);
 
 -- --------------------------------------------------------
 
@@ -227,17 +257,6 @@ ALTER TABLE `billing`
   ADD KEY `billID` (`billID`);
 
 --
--- Indexes for table `file`
---
-ALTER TABLE `file`
-  ADD PRIMARY KEY (`fileID`),
-  ADD UNIQUE KEY `patientID` (`patientID`),
-  ADD UNIQUE KEY `lastModifiedID` (`lastModifiedID`),
-  ADD KEY `patientID_2` (`patientID`),
-  ADD KEY `refPhysicianID` (`refPhysicianID`),
-  ADD KEY `fileID` (`fileID`);
-
---
 -- Indexes for table `images`
 --
 ALTER TABLE `images`
@@ -260,7 +279,8 @@ ALTER TABLE `login`
 ALTER TABLE `patient`
   ADD PRIMARY KEY (`patientID`),
   ADD UNIQUE KEY `patientID` (`patientID`),
-  ADD KEY `patientID_2` (`patientID`);
+  ADD KEY `patientID_2` (`patientID`),
+  ADD KEY `refPhysicianID` (`refPhysicianID`);
 
 --
 -- Indexes for table `presentstaff`
@@ -290,10 +310,10 @@ ALTER TABLE `report`
   ADD PRIMARY KEY (`reportID`),
   ADD UNIQUE KEY `reportID` (`reportID`),
   ADD KEY `lastModifiedID` (`lastModifiedID`),
-  ADD KEY `fileID` (`fileID`),
+  ADD KEY `fileID` (`patientID`),
   ADD KEY `reportID_2` (`reportID`),
   ADD KEY `lastModifiedID_2` (`lastModifiedID`),
-  ADD KEY `fileID_2` (`fileID`);
+  ADD KEY `fileID_2` (`patientID`);
 
 --
 -- Indexes for table `schedule`
@@ -342,14 +362,6 @@ ALTER TABLE `billing`
   ADD CONSTRAINT `billing_ibfk_1` FOREIGN KEY (`patientID`) REFERENCES `patient` (`patientID`) ON UPDATE CASCADE;
 
 --
--- Constraints for table `file`
---
-ALTER TABLE `file`
-  ADD CONSTRAINT `file_ibfk_1` FOREIGN KEY (`patientID`) REFERENCES `patient` (`patientID`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `file_ibfk_2` FOREIGN KEY (`lastModifiedID`) REFERENCES `staff` (`staffID`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `file_ibfk_3` FOREIGN KEY (`refPhysicianID`) REFERENCES `refphysician` (`refPhysicianID`) ON DELETE NO ACTION ON UPDATE CASCADE;
-
---
 -- Constraints for table `images`
 --
 ALTER TABLE `images`
@@ -363,6 +375,12 @@ ALTER TABLE `login`
   ADD CONSTRAINT `login_ibfk_1` FOREIGN KEY (`staffID`) REFERENCES `staff` (`staffID`) ON UPDATE CASCADE;
 
 --
+-- Constraints for table `patient`
+--
+ALTER TABLE `patient`
+  ADD CONSTRAINT `patient_ibfk_1` FOREIGN KEY (`refPhysicianID`) REFERENCES `staff` (`staffID`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
 -- Constraints for table `presentstaff`
 --
 ALTER TABLE `presentstaff`
@@ -372,14 +390,14 @@ ALTER TABLE `presentstaff`
 -- Constraints for table `radorder`
 --
 ALTER TABLE `radorder`
-  ADD CONSTRAINT `radorder_ibfk_1` FOREIGN KEY (`refPhysicianID`) REFERENCES `refphysician` (`refPhysicianID`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `radorder_ibfk_1` FOREIGN KEY (`refPhysicianID`) REFERENCES `staff` (`staffID`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `report`
 --
 ALTER TABLE `report`
-  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`fileID`) REFERENCES `file` (`fileID`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `report_ibfk_2` FOREIGN KEY (`lastModifiedID`) REFERENCES `staff` (`staffID`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `report_ibfk_2` FOREIGN KEY (`lastModifiedID`) REFERENCES `staff` (`staffID`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `report_ibfk_3` FOREIGN KEY (`patientID`) REFERENCES `patient` (`patientID`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `schedule`
