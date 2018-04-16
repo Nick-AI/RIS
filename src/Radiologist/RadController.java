c2292b5b0b8ac0d7c4e2198281c7e8bb8886cf3c
Nick-AI <31321017+Nick-AI@users.noreply.github.com>
2018-04-16 04:11:45 -0400

    Add files via upload

---
+++ src/Radiologist/RadController.java
@@ -0,0 +1,13 @@
+package Radiologist; 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class RadController {
+	
+	@FXML
+	private TableColumn welctableLbl1;
+	private TableView procedureTableID;
+
+}
